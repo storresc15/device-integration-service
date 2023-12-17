5e31370fcb9dcaf323d9db0eed0c3447dcbd52d8
Sebastian Torres <sebastiantorres@Sebastians-MacBook-Pro.local>
2023-12-17 00:15:16 -0600

    Finished the user test class

--- src/test/java/com/smda/deviceint/deviceintegration/UserServiceTest.java
+++ src/test/java/com/smda/deviceint/deviceintegration/UserServiceTest.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -33,4 +37,72 @@ public class UserServiceTest {
         assertTrue(retrievedUser.isPresent());
         assertEquals(user.getUsername(), retrievedUser.get().getUsername());
     }
+    @Test
+    void testGetAllUsers() {
+        Long userId = 1L;
+        User user = new User("JohnDoe", "JohnDoe@test.com");
+        user.setUserId(userId);
+        List<User> users = new ArrayList<User>();
+        users.add(user);
+        when(userRepository.findAll()).thenReturn(users);
+
+        List<User> retrievedUsers = userService.getAllUsers();
+        assertFalse(retrievedUsers.isEmpty());
+        assertEquals(user.getUsername(), retrievedUsers.get(0).getUsername());
+    }
+
+    @Test
+    void testCreateUser() {
+        // Given
+        User userToCreate = new User("JohnDoe", "JohnDoe@test.com");
+        User createdUser = new User("JohnDoe", "JohnDoe@test.com");
+
+        // Mocking the repository behavior
+        when(userRepository.save(userToCreate)).thenReturn(createdUser);
+
+        // When
+        User savedUser = userService.createUser(userToCreate);
+
+        // Then
+        assertEquals(createdUser.getUserId(), savedUser.getUserId());
+        assertEquals(userToCreate.getUsername(), savedUser.getUsername());
+    }
+
+    @Test
+    void testUpdateUser() throws ChangeSetPersister.NotFoundException {
+        // Given
+        Long userId = 1L;
+        User existingUser = new User("JohnDoe", "JohnDoe@test.com");
+        existingUser.setUserId(userId);
+        User updatedUser = new User("JohnDoe1", "JohnDoe@test.com");
+        updatedUser.setUserId(userId);
+
+        // Mocking the repository behavior
+        when(userRepository.existsById(userId)).thenReturn(Boolean.TRUE);
+        when(userRepository.save(updatedUser)).thenReturn(updatedUser);
+
+        // When
+        User result = userService.updateUser(userId, updatedUser);
+
+        // Then
+        assertEquals(updatedUser.getUsername(), result.getUsername());
+        verify(userRepository).save(updatedUser);
+    }
+
+    @Test
+    void testDeleteUser() throws ChangeSetPersister.NotFoundException {
+        // Given
+        Long userId = 1L;
+
+        // Mocking the repository behavior
+        when(userRepository.existsById(userId)).thenReturn(true);
+
+        // When
+        userService.deleteUser(userId);
+
+        // Then
+        //assertTrue(deleted);
+        verify(userRepository).deleteById(userId);
+    }
+
 }
